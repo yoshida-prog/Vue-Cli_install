5696dd9b2d559d9b749d1580e1637bdab62cbd93
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-14 20:05:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: '#app',
+  data(){
+    return{
+      hello: 'Hello, World!'
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  color: red
+}
+</style>
